6051e0b113d6eb0f3054289e432783aab12ab5fe
Bharath1121 <bsenthil@calpoly.edu>
2022-05-17 17:12:39 -0700

    Finished FIFO

--- memSim.py
+++ memSim.py
@@ -1,7 +1,8 @@
 import sys
 
-TLB = []
-pageTable = {}
+tlb = [] # [(logical memory page num, physical memory frame number), ...]
+pageTable = {} # pageNum: [FrameNum, validBit]
+physicalMemory = [] # (frameNum, page)
 
 def main():
     inputFile = sys.argv[1]
@@ -12,14 +13,10 @@ def main():
         algorithm = "FIFO"
     if frameNums.isdigit() == False or int(frameNums) <= 0 or int(frameNums) > 256:
         frameNums = 256
-    # print(inputFile, frameNums, algorithm)
+    for j in range(256):
+        pageTable[j] = None
     addressList = readFile(inputFile)
-    if algorithm == "FIFO":
-        doFIFO(addressList, frameNums)
-    elif algorithm == "LRU":
-        doLRU(addressList, frameNums)
-    elif algorithm == "OPT":
-        doOPT(addressList, frameNums)
+    driver(addressList, frameNums, algorithm)
 
 def readFile(inputFile):
     try:
@@ -33,32 +30,93 @@ def readFile(inputFile):
         del listOfAddresses[len(listOfAddresses) - 1]
     return listOfAddresses
 
-def doFIFO(addressList, numOfFrames):
+def updateTLB(pageNum, frameNumber):
+    if len(tlb) < 16:
+        tlb.append((pageNum, frameNumber))
+    elif len(tlb) == 16:
+        tlb.pop(0)
+        tlb.append((pageNum, frameNumber))
+
+def updatePM(pageNum, frameNumber, numOfFrames, algorithm):
+    if len(physicalMemory) < numOfFrames:
+        physicalMemory.append((frameNumber, pageNum))
+        frameNumber += 1
+    elif len(physicalMemory) == numOfFrames:
+        if algorithm == "FIFO":
+            frameNumber = doFIFO(physicalMemory, pageNum)
+        elif algorithm == "LRU":
+            frameNumber = doLRU(physicalMemory, pageNum, frameNumber)
+        elif algorithm == "OPT":
+            frameNumber = doOPT(physicalMemory, pageNum, frameNumber)
+    return frameNumber
+
+def driver(addressList, numOfFrames, algorithm):
+    frameNumber = 0
+    pageFaultCount = 0
+    missCount = 0
+    hitCount = 0
+    numOfFrames = int(numOfFrames)
     f = open("BACKING_STORE.bin", "rb")
     for address in addressList:
         f.seek(int(address))
         byteReferenced = f.read(1)
         byteReferenced = int(byteReferenced.hex(), 16)
+        pageNum = int(address) // 256
         if byteReferenced >= 128:
             byteReferenced = byteReferenced - 256
         f.seek((int(address)//256)*256)
         contents = f.read(256)
-        print(address + ", " + str(byteReferenced) + ", " + "__")
+
+        hitCountBefore = hitCount
+        for ting in tlb:
+            if pageNum == ting[0]:
+                hitCount += 1
+                break
+        if hitCountBefore == hitCount: # Not a hit
+            if pageTable[pageNum] == None or pageTable[pageNum][1] == 0: # Page Fault
+                pageFaultCount += 1
+                missCount += 1
+                pageTable[pageNum] = (frameNumber, 1)
+                updateTLB(pageNum, frameNumber)
+                frameNumber = updatePM(pageNum, frameNumber, numOfFrames, algorithm)
+            else: # Soft Miss
+                missCount += 1
+                updateTLB(pageNum, frameNumber)
+                frameNumber = updatePM(pageNum, frameNumber, numOfFrames, algorithm)
+    
+        # print("TLB:",tlb)
+        # print("Physical Memory Representation", physicalMemory)
+        for item in physicalMemory:
+            if item[1] == pageNum:
+                finalFrameValue = item[0]
+        print(address + ", " + str(byteReferenced) + ", " + str(finalFrameValue))
         print(contents.hex())
+
     print("Number of Translated Addresses = ", len(addressList))
-    print("Page Faults = ")
-    print("Page Fault Rate = ")
-    print("TLB Hits = ")
-    print("TLB Misses = ")
-    print("TLB Hit Rate = ")
+    print("Page Faults =", pageFaultCount)
+    print("Page Fault Rate = %3.3f" %(pageFaultCount/len(addressList)))
+    print("TLB Hits =", hitCount)
+    print("TLB Misses =", missCount)
+    print("TLB Hit Rate = %3.3f" %(hitCount/len(addressList)))
 
-def doLRU(addressList, numOfFrames):
+
+def doFIFO(physicalMemory, pageNum):
+    invalidPageNum = physicalMemory.pop(0)
+    physicalMemory.append((invalidPageNum[0], pageNum))
+    pageTable[invalidPageNum[1]] = (invalidPageNum[0], 0)
+    for i in range(len(tlb)):
+        if tlb[i][0] == invalidPageNum[1]:
+            del tlb[i]
+            break
+    # print("You are in FIFO function.")
+    return invalidPageNum[0]
+
+def doLRU(physicalMemory):
     print("You are in LRU function.")
 
-def doOPT(addressList, numOfFrames):
-    print("You are in LRU function.")
+def doOPT(physicalMemory):
+    print("You are in OPT function.")
 
 
 if __name__ == '__main__':
     main()
-    
